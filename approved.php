42affc01fb5348acb93263dd1c2cc8b57069034f
Talal <35473049+talsai@users.noreply.github.com>
2019-01-30 14:02:34 -0500

    Add files via upload

--- approved.php
+++ approved.php
@@ -13,11 +13,13 @@ $sql = "UPDATE all_contacts SET status='Approved' WHERE id = '$id'"; //query : u
 
 $url = 'https://stage-tms.govdelivery.com/messages/email';
 $data = array(
-  "subject" => "Hello!",
+  "subject" => "Hello World!",
   "body" => "hi",
   "recipients" => array (
-    "email" => "talal.said@granicus.com")
+    "email" => "talal.said@granicus.com",
+    )
   );
+
 $data_string = json_encode($data);
 $ch = curl_init($url);
 
@@ -25,23 +27,18 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_CAINFO, '/Applications/AMPPS/extra/etc/openssl/certs/cacert.pem');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "X-AUTH-TOKEN: xxxxxxxxx",
+    "Accept: application/json",
     "Content-Type: application/json",
-    "X-AUTH-TOKEN: xxxxx",
+    "charset: utf-8",
     "Content-Length: ". strlen($data_string)));
-
 
 $result = curl_exec($ch);
 echo $result;
 print_r(curl_getinfo($ch));
+print curl_error($ch);
 curl_close($ch);
-
-if (mysqli_query($conn, $sql)) {
-    mysqli_close($conn);
-    header("Location: dashboard.php"); // on success delete : redirect the page to original page using header() method
-    exit;
-} else {
-    echo "Error changing status";
-}
 
 ?>
